3eb74769ea4d5c96f1cb73bddd41dcf2e6381461
Christophe Coevoet <stof@notk.org>
2022-11-23 15:15:40 +0100

    Fix typescript warning in the simple-mode type declaration

--- mode/simple-mode.d.ts
+++ mode/simple-mode.d.ts
@@ -1,7 +1,7 @@
 import {StreamParser} from "@codemirror/language"
 export interface Rule {
   regex?: string | RegExp | undefined;
-  token?: string | string[] | null | undefined | ((RegExpMatchArray) => string | string[] | null);
+  token?: string | string[] | null | undefined | ((matches: RegExpMatchArray) => string | string[] | null);
   sol?: boolean | undefined;
   next?: string | undefined;
   push?: string | undefined;
