498e4233f6236cdbea10ec88c8fc8ff3998ac40a
Reza Salkhordeh <reza.salkhordeh@gmail.com>
2022-12-06 14:37:16 +0100

    Add Farsi characters to command detection

--- mode/stex.js
+++ mode/stex.js
@@ -81,7 +81,7 @@ function mkStex(mathMode) {
   function normal(source, state) {
     var plug;
     // Do we look like '\command' ?  If so, attempt to apply the plugin 'command'
-    if (source.match(/^\\[a-zA-Z@]+/)) {
+    if (source.match(/^\\[a-zA-Z@\u0622\u0627\u0628\u067E\u062A-\u062C\u0686\u062D-\u0632\u0698\u0633-\u063A\u0641\u0642\u06A9\u06AF\u0644-\u0648\u06CC]+/)) {
       var cmdName = source.current().slice(1);
       plug = plugins.hasOwnProperty(cmdName) ? plugins[cmdName] : plugins["DEFAULT"];
       plug = new plug();
