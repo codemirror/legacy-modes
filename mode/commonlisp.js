73ca036984cb9b87fbd297f78b51fdc138c63e64
Qi Ji <qjqqyy@users.noreply.github.com>
2024-04-09 11:40:07 +0800

    Update commonlisp.js
    stop recognising ] as closing bracket (it's a symbol)

--- mode/commonlisp.js
+++ mode/commonlisp.js
@@ -20,8 +20,8 @@ function base(stream, state) {
   if (ch == "\\") ch = stream.next();
 
   if (ch == '"') return (state.tokenize = inString)(stream, state);
-  else if (ch == "(" || ch == "[") { type = "open"; return "bracket"; }
-  else if (ch == ")" || ch == "]") { type = "close"; return "bracket"; }
+  else if (ch == "(") { type = "open"; return "bracket"; }
+  else if (ch == ")") { type = "close"; return "bracket"; }
   else if (ch == ";") { stream.skipToEnd(); type = "ws"; return "comment"; }
   else if (/['`,@]/.test(ch)) return null;
   else if (ch == "|") {
